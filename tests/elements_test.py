bbfda28e291adfed7f975b2afe8dff30ed2826a3
si3pi2e <si314pi272e@gmail.com>
2023-01-01 01:45:37 +0100

    fixed and add code for video 6

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,5 +1,5 @@
 import time
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -14,3 +14,16 @@ class TestElements:
             assert email == output_email, "the email does not match."
             assert current_address == output_current_address, "the current address does not match."
             assert permanent_address == output_permanent_address, "the permanent address does not match."
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, url='https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+
+            time.sleep(1)
+            input_check_box = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+
+            assert input_check_box == output_result, "choice checkboxes have not been selected"
